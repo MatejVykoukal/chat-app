3d4e1632463babcde13dfdb049b71c99b3127b62
Matej Vykoukal <61636415+MatejVykoukal@users.noreply.github.com>
2022-09-02 11:30:07 +0200

    fix: typo in package name

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Server } from 'Socket.IO';
+import { Server } from 'socket.io';
 import { Message } from '../../types';
 
 const SocketHandler = (req, res) => {
